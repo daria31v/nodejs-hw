b4bd226913eb44fd5808529d5e1b2668a07475f0
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-04-28 18:50:30 +0200

    commit 3
    main tasks

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
-
+const Jimp = require("jimp");
 
 const { User } = require("../models/user");
 const { SECRET_KEY } = process.env;
@@ -91,18 +91,24 @@ const updateSubscription = async (req, res) => {
 
 const updateAvatar = async (req, res) => {
   const { token, _id } = req.user;
-  console.log(token);
   const { path: tempUpload, originalname } = req.file;
   const filename = `user_-_${_id}_${originalname}`;
-
   const resultUpload = path.join(avatarDir, filename);
   await fs.rename(tempUpload, resultUpload);
 
   const avatarURL = path.join("avatars", resultUpload);
   await User.findOneAndUpdate(token, { avatarURL });
+  
+  (async function () {
+    const image = await Jimp.read(resultUpload)
+    image.resize(250, 250);
+    image.write(
+      resultUpload
+    );
+  })();
 
   res.status(200).json({
-    avatarURL
+    avatarURL,
   });
 };
 
--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const HttpError = require('./HttpError');
 const ctrWrapper = require('./ctrWrapper');
 const handleMongooseError = require('./handleMongooseError');
+// const resize = require('./jipm');
 
 module.exports = { HttpError, ctrWrapper, handleMongooseError}
